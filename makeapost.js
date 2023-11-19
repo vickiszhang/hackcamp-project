299a8886c9008f33469167888799afede400534f
tracygan <tracyygann@gmail.com>
2023-11-19 13:54:37 -0800

    new changes

---
+++ makeapost.js
@@ -0,0 +1,11 @@
+// function openDatePicker() {
+//   $("#datepicker-input").datepicker("show");
+// }
+
+// $(document).ready(function () {
+//   $("#datepicker-input").datepicker({
+//     format: "yyyy-mm-dd",
+//     autoclose: true,
+//     todayHighlight: true,
+//   });
+// });
